cd8021bbe2419d89281c72150bce66e649e51563
YaKem <ykemouche@gmail.com>
2019-10-28 16:23:05 +0100

    changement

--- page.php
+++ page.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>ADMIN SPACE</title>
 </head>
 <body>
     <header><a href="index.php?r=deconnect">Logout</a></a></header>
